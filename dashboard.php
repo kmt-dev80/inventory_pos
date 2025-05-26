9ab0216aaf1ad47f5bcd40acfb4d49113132e1d1
AfsanaSohana <sohanaafsana16@gmail.com>
2025-05-26 13:09:27 +0600

    wrong file replaced

---
+++ dashboard.php
@@ -0,0 +1,9 @@
+ <?php require_once __DIR__ . '/requires/header.php'; ?>
+ <?php require_once __DIR__ . '/requires/sidebar.php'; ?>
+ <?php require_once __DIR__ . '/requires/topbar.php'; ?>
+<div class="container">
+    <div class="page-inner">
+           <h2>Welcome to the Dashboard</h2>
+    </div>
+</div>
+ <?php require_once __DIR__ . '/requires/footer.php'; ?>
