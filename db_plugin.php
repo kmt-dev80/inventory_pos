b61b1a9c2cf168dd6cd1b32f8773ad815ec1b9ea
kmt-dev80 <mdtakiulhasan@gmail.com>
2025-06-24 12:39:43 +0600

    git_ignore

--- db_plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors', 0);
-ini_set('error_log', __DIR__ . '/error.log');
-date_default_timezone_set('Asia/Dhaka');
-define('CURRENCY', '৳');
-define('BASE_URL', 'http://localhost/inventory_pos/');
-require_once __DIR__ . '/includes/crud_class.php';
-$mysqli=new CRUD();
-?>
